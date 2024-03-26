927ba5b17419dcffb393d670bb3c03fb6634cd8f
Lynda <lylyben74@gmail.com>
2024-03-26 22:17:31 +0100

    add interaction

--- src/main.js
+++ src/main.js
@@ -7,3 +7,31 @@ console.log(scene)
 
 const scene2 = new Scene("canvas")
 console.log(scene2)
+
+// animation
+
+const canvas = document.getElementById('canvas');
+
+
+canvas.addEventListener('mouseover', function() {
+    document.getElementById('background').style.opacity = 1; // Fait apparaître progressivement le fond d'écran
+    playMusic()
+});
+
+canvas.addEventListener('mouseout', function() {
+    document.getElementById('background').style.opacity = 0; // Fait disparaître progressivement le fond d'écran
+    stopMusic()
+});
+
+// Fonction pour jouer la musique
+function playMusic() {
+    const audio = document.getElementById('audio');
+    audio.play();
+}
+
+// Fonction pour arrêter la musique
+function stopMusic() {
+    const audio = document.getElementById('audio');
+    audio.pause();
+    audio.currentTime = 0; // Rembobine la musique au début
+}
