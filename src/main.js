f34f9ae9f812b967c12fd44cdd4f8ecbef33d8c1
Lynda <lylyben74@gmail.com>
2024-03-27 22:50:39 +0100

    add music

--- src/main.js
+++ src/main.js
@@ -25,10 +25,41 @@ canvas.addEventListener('mouseout', function() {
     stopMusic()
 });
 
-// Fonction pour jouer la musique
+
+// Fonction pour jouer la musique aléatoirement
 function playMusic() {
+    const randomMusic = Math.floor(Math.random() * 3);
+
     const audio = document.getElementById('audio');
-    audio.play();
+
+    if (audio) {
+
+        document.querySelectorAll('audio').forEach(a => {
+            if (a !== audio) {
+                a.pause();
+            }
+        });
+
+        audio.src = audio.children[randomMusic].src;
+
+        audio.play();
+
+        switch (randomMusic) {
+            case 0:
+                document.getElementById('background').style.backgroundImage = "url('https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbzJtempmZXgxamc2bXFlb2hxaWxxamF5MXVuejF5NnN1OGJ1N3RjYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hrrcWb7OpDoxhgR4xB/giphy.gif')";
+                break;
+            case 1:
+                document.getElementById('background').style.backgroundImage = "url('https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjY3Y3k2eXJvZWsxemd1NXoxaGVpaDJrZ3o5MzlicnV2ZTI5Y2dscCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/exfSTanP4prYQ/giphy.gif')";
+                break;
+            case 2:
+                document.getElementById('background').style.backgroundImage = "url('https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExODR0emo3Ymgzb3prODltNzU2dDQzbHNzZzRhNmF3aDhqNnRvYzk5YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/pJmnk86fXFNmrUb8LB/giphy.gif')";
+                break;
+            default:
+                document.getElementById('background').style.backgroundColor = "white";
+        }
+
+    }
+
 }
 
 // Fonction pour arrêter la musique
@@ -37,3 +68,5 @@ function stopMusic() {
     audio.pause();
     audio.currentTime = 0; // Rembobine la musique au début
 }
+
+
